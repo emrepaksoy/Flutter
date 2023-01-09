e9961f37850532c7d23e084ac692abe2516f11f0
Emre Paksoy <55803656+emrepaksoy@users.noreply.github.com>
2023-01-09 17:32:23 +0300

    scaffol edit

--- learning_app/lib/101/scaffold_learn.dart
+++ learning_app/lib/101/scaffold_learn.dart
@@ -13,8 +13,19 @@ class ScaffoldLearnView extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: const Center(child: Text("Hello Flutter")),
-      backgroundColor: Colors.amber,
+      // body: const Center(child: Text("Hello Flutter")),
+      body: Stack(
+        children: const [Background(), Center(child: Text("Hello Flutter"))],
+      ),
+      // body: Container(
+      //   decoration: ProjectContainerDecoration(),
+      //   child: Column(
+      //     children: const [Center(child: Text("Hello Flutter"))],
+      //   ),
+      // ),
+
+      //backgroundColor: Colors.amber,
+
       extendBody: true,
       //floatingActionButton: FloatingActionButton(onPressed: () {}),
       floatingActionButton: FloatingActionButton(onPressed: () {
@@ -24,22 +35,49 @@ class ScaffoldLearnView extends StatelessWidget {
                   height: 200,
                 ));
       }),
-      
+
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       drawer: const Drawer(),
       //bottomSheet: const Text('data'),
       bottomNavigationBar: Container(
-        height: 200,
-        decoration:ProjectContainerDecoration(),
-        child: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.abc_outlined), label: 'button 1'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.abc_outlined), label: 'button 2')
-          ],
-        ),
+        
+        margin: const EdgeInsets.all(10),
+        height: 100,
+        decoration: ProjectContainerDecoration(),
+        child: const BottomBar(),
       ),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.red, Colors.orange])),
+    );
+  }
+}
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      
+      selectedFontSize: 15,
+      unselectedFontSize: 10,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'button 1'),
+        BottomNavigationBarItem(icon: Icon(Icons.usb), label: 'button 2')
+      ],
+      unselectedItemColor: Colors.blueAccent,
+      selectedItemColor: Colors.greenAccent,
+      showSelectedLabels: true,
+
     );
   }
 }
